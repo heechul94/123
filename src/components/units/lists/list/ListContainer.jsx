a0933fe4330d62e72fe9090623063f27a1430462
heechul94 <yhcu3517@gmail.com>
2024-01-30 03:33:24 +0900

    Fix: 펜레터 Link적용

--- src/components/units/lists/list/ListContainer.jsx
+++ src/components/units/lists/list/ListContainer.jsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom";
 import ListPresenter from "./ListPresenter";
 
 const ListContainer = ({ item }) => {
-    return <ListPresenter item={item} />;
+    return (
+        <Link to={`detail/${item.id}`}>
+            <ListPresenter item={item} />
+        </Link>
+    );
 };
 export default ListContainer;
