2c1f0128f9d5d77cb2b4d6c83752bda169aae887
heechul94 <yhcu3517@gmail.com>
2024-01-30 03:37:26 +0900

    :sparkles: Feat: 상세페이지 Read Delete 기능 추가

--- src/components/units/detail/DetailContainer.jsx
+++ src/components/units/detail/DetailContainer.jsx
@@ -1,6 +1,23 @@
+import { useNavigate, useParams } from "react-router-dom";
 import DetailPresenter from "./DetailPresenter";
 
 const DetailContainer = () => {
-    return <DetailPresenter />;
+    const data = JSON.parse(localStorage.getItem("fanLetters"));
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const articleData = data.find((item) => item.id === id);
+    const onClickDeleteArticleButtonHandler = () => {
+        const deletedData = data.filter((item) => item !== articleData);
+        localStorage.setItem("fanLetters", JSON.stringify(deletedData));
+        navigate("/");
+    };
+    return (
+        <DetailPresenter
+            {...articleData}
+            onClickDeleteArticleButtonHandler={
+                onClickDeleteArticleButtonHandler
+            }
+        />
+    );
 };
 export default DetailContainer;
--- src/components/units/detail/DetailPresenter.jsx
+++ src/components/units/detail/DetailPresenter.jsx
@@ -1,4 +1,44 @@
-const DetailPresenter = () => {
-    return <div>상세페이지 프레젠터</div>;
+import Button from "components/commons/buttons/Button";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const DetailWrapper = styled.main`
+    margin: 50% auto;
+    border: 1px solid black;
+`;
+const ArticleWrapper = styled.article`
+    border: 1px solid black;
+`;
+const ArticleHeader = styled.div`
+    border-bottom: 1px solid black;
+`;
+
+const DetailPresenter = ({
+    nickName,
+    createdAt,
+    content,
+    onClickDeleteArticleButtonHandler,
+}) => {
+    return (
+        <DetailWrapper>
+            <Link to="/">
+                <div>홈으로</div>
+            </Link>
+            <ArticleWrapper>
+                <ArticleHeader>
+                    <span>{nickName}</span>
+                    <span>{createdAt}</span>
+                </ArticleHeader>
+                <div>{content}</div>
+                <Button text="수정" />
+                <Button
+                    text="삭제"
+                    onClickDeleteArticleButtonHandler={
+                        onClickDeleteArticleButtonHandler
+                    }
+                />
+            </ArticleWrapper>
+        </DetailWrapper>
+    );
 };
 export default DetailPresenter;
