c34de96c327786501230b3b76dd7b617cdd3c963
heechul94 <yhcu3517@gmail.com>
2024-01-30 17:49:30 +0900

    :truck: Chore: 상세 수정페이지 라우터 연결

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "../components/commons/layouts/Layout";
 import Home from "../pages/Home";
 import Detail from "pages/Detail";
+import EditDetail from "pages/EditDetail";
 const Router = () => {
     return (
         <BrowserRouter>
@@ -10,6 +11,7 @@ const Router = () => {
                     <Route path="/" element={<Home />} />
                 </Route>
                 <Route path="detail/:id" element={<Detail />} />
+                <Route path="editDetail/:id" element={<EditDetail />} />
             </Routes>
         </BrowserRouter>
     );
